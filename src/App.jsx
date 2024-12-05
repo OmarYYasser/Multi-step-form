955e31176c9557f50d94ece2d229f2e210b13d4e
omar yasser <136465139+OmarYYasser@users.noreply.github.com>
2024-12-05 03:53:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import SpinnerPage from "./components/SpinnerPage";
+import { Suspense } from "react";
 
 const PersonalInfo = lazy(() => import("./pages/PersonalInfo"));
 const SelectPlan = lazy(() => import("./pages/SelectPlan"));
 const Pick = lazy(() => import("./pages/Pick"));
 const FinishUp = lazy(() => import("./pages/FinishUp"));
 const Confirm = lazy(() => import("./pages/Confirm"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<SpinnerPage />}>
         <Routes>
-          <Route
-            index
-            path="/"
-            element={<Navigate replace to="personal-info" />}
-          />
-          <Route index path="personal-info" element={<PersonalInfo />} />
-          <Route index path="select-plan" element={<SelectPlan />} />
-          <Route index path="pick" element={<Pick />} />
-          <Route index path="finish-up" element={<FinishUp />} />
-          <Route index path="confirm" element={<Confirm />} />
+          <Route path="/" element={<Navigate replace to="personal-info" />} />
+
+          <Route path="personal-info" element={<PersonalInfo />} />
+          <Route path="select-plan" element={<SelectPlan />} />
+          <Route path="pick" element={<Pick />} />
+          <Route path="finish-up" element={<FinishUp />} />
+          <Route path="confirm" element={<Confirm />} />
+
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
